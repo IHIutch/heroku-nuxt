91f1d9b5e5f32f9ed8476e206776a7baa288bf88
IHIutch <jbhutch01@gmail.com>
2020-07-05 17:28:06 -0400

    Update stop page

--- pages/stops/_StopId.vue
+++ pages/stops/_StopId.vue
@@ -1,8 +1,109 @@
 <template>
-  <b-container>
+  <b-container class="py-5">
     <b-row>
-      <b-col>
-        {{ stop }}
+      <b-col cols="8" offset="2">
+        <div class="mb-5">
+          <div class="d-flex align-items-center justify-content-between">
+            <h1>{{ stop.stopName }}</h1>
+            <b-button variant="primary" size="sm">Take Survey</b-button>
+          </div>
+          <div>
+            <span class="mr-3">Stop Code: {{ stop.stopCode }}</span>
+            <span class="text-muted">
+              Coords: {{ stop.stopLat }}, {{ stop.stopLon }}
+            </span>
+          </div>
+        </div>
+        <!-- <b-row>
+          <b-col v-for="(cat, idx) in questionByCategory" :key="idx">
+            <div class="text-center mb-5">
+              <DoughnutChartWrapper
+                :foregroundColor="getChartForegroundColor(cat.score)"
+                :backgroundColor="getChartBackgroundColor(cat.score)"
+                :percent="cat.score"
+                :thickness="20"
+              >
+                <div class="mx-auto">
+                  <div class="mb-3">
+                    <span class="h1">{{ cat.score }}</span>
+                  </div>
+                  <div>
+                    <span>{{ cat.text }}</span>
+                  </div>
+                </div>
+              </DoughnutChartWrapper>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <div
+              v-for="(cat, catIdx) in questionByCategory"
+              :key="catIdx"
+              class="mb-4"
+            >
+              <div class="d-flex justify-content-between mb-2">
+                <h3 class="mb-0">{{ cat.text }}</h3>
+              </div>
+              <div role="tablist" class="border border-bottom-0">
+                <b-card
+                  no-body
+                  border-variant="0"
+                  v-for="(question, questionIdx) in cat.questions"
+                  :key="questionIdx"
+                >
+                  <b-card-header
+                    header-tag="header"
+                    class="px-0 py-1 bg-transparent"
+                    role="tab"
+                  >
+                    <b-button
+                      block
+                      v-b-toggle="cat.text + '-' + questionIdx"
+                      class="d-flex justify-content-between text-decoration-none py-0 text-dark"
+                      variant="link"
+                    >
+                      <div>
+                        <span class="font-weight-bold">
+                          {{ question.text }}
+                        </span>
+                      </div>
+                      <div class="d-flex">
+                        <div class="mr-3">
+                          <span v-if="getResult()">
+                            <b-badge variant="success">True</b-badge>
+                          </span>
+                          <span v-else>
+                            <b-badge variant="danger">False</b-badge>
+                          </span>
+                        </div>
+                        <div>
+                          <span>
+                            <chevron-down-icon size="1.5x" />
+                          </span>
+                        </div>
+                      </div>
+                    </b-button>
+                  </b-card-header>
+                  <b-collapse
+                    class="border-bottom"
+                    :id="cat.text + '-' + questionIdx"
+                    accordion="somethings"
+                    role="tabpanel"
+                  >
+                    <div class="p-3">
+                      <p class="mb-0">
+                        This is a description of the question and why the
+                        question is important. There might even be a
+                        <a href="#">link to a study</a> about this topic.
+                      </p>
+                    </div>
+                  </b-collapse>
+                </b-card>
+              </div>
+            </div>
+          </b-col>
+        </b-row> -->
       </b-col>
     </b-row>
   </b-container>
@@ -18,6 +119,11 @@ export default {
         {
           hid: "title",
           name: "title",
+          content: this.stop.stopName,
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
           content: this.stop.stopName,
         },
         {
@@ -39,6 +145,25 @@ export default {
       .catch((err) => {
         error({ statusCode: 404, message: "Stop not found" });
       });
+  },
+  methods: {
+    getResult() {
+      return Math.random() < 0.5;
+    },
+    navigateToCategory(category) {
+      this.$router.push({
+        name: "StopCategoryEditPage",
+        params: {
+          category: category.title,
+        },
+      });
+    },
+    getChartForegroundColor(score) {
+      return getColorByNumber(score);
+    },
+    getChartBackgroundColor(score) {
+      return getColorByNumber(score, 0.2);
+    },
   },
 };
 </script>
