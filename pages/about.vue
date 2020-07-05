0c39e0200ca3c10cc2d6cb2436218f1f386db302
IHIutch <jbhutch01@gmail.com>
2020-07-05 19:50:10 -0400

    Show case change

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>About</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  head() {
+    return {
+      title: this.pageTitle,
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: 'About Page?',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Really the about page',
+        },
+      ],
+    }
+  },
+  asyncData() {
+    return { pageTitle: 'About!!' }
+  },
+  data() {
+    return {
+      pageTitle: '',
+    }
+  },
+}
+</script>
