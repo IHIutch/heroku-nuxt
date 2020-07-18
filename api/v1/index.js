fe8fdd7756db081ad0de7db8682d38a0927be248
IHIutch <jbhutch01@gmail.com>
2020-07-17 22:10:13 -0400

    Install cors

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+const origin =
+  process.env.NODE_ENV === "production"
+    ? "http://nuxt.herokuapp.com"
+    : "http://localhost:3000";
 
 app.use(express.json());
+app.use(cors({
+  origin: origin
+}))
 
 app.use("/auth", require("./routes/auth"));
 app.use("/stops", require("./routes/stops"));
