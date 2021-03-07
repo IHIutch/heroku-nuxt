c9b86fb80c2b0a9faf1b51ca2ce70111cb973062
IHIutch <jbhutch01@gmail.com>
2021-03-07 16:22:51 -0500

    update login

--- pages/login.vue
+++ pages/login.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="mt-12">
-    <b-container>
-      <b-row>
-        <b-col cols="6" offset="3">
-          <div class="shadow p-4 rounded bg-white">
-            <b-form @submit.prevent="logIn()">
-              <b-form-group label="Email" label-for="email">
-                <b-form-input
-                  id="email"
-                  v-model="form.email"
-                  type="email"
-                  required
-                  placeholder="Enter name"
-                ></b-form-input>
-              </b-form-group>
-              <b-form-group label="Password" label-for="password">
-                <b-form-input
-                  id="password"
-                  v-model="form.password"
-                  type="password"
-                  required
-                  placeholder="Enter password"
-                ></b-form-input>
-              </b-form-group>
-              <b-button type="submit" variant="primary">Log In</b-button>
-            </b-form>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+  <Container v-chakra my="12">
+    <CGrid template-columns="repeat(12, 1fr)" gap="6">
+      <CBox grid-column-start="4" grid-column-end="10">
+        <CBox shadow="sm" p="6" rounded="md" bg="white">
+          <form @submit.prevent="logIn()">
+            <CFormControl mb="4">
+              <CFormLabel for="email">Email address</CFormLabel>
+              <CInput
+                id="email"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter name"
+              />
+            </CFormControl>
+            <CFormControl mb="4">
+              <CFormLabel for="password">Password</CFormLabel>
+              <CInput
+                id="password"
+                v-model="form.password"
+                type="password"
+                required
+                placeholder="Enter password"
+              />
+            </CFormControl>
+            <CButton type="submit">Log In</CButton>
+          </form>
+        </CBox>
+      </CBox>
+    </CGrid>
+  </Container>
 </template>
 
 <script>
+import Container from '~/components/global/Container.vue'
 export default {
   name: 'Login',
+  components: { Container },
   data() {
     return {
       form: {
