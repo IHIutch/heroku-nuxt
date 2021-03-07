59008670588847aa259e400f645efe61d20d158c
IHIutch <jbhutch01@gmail.com>
2021-03-06 23:22:37 -0500

    Temp local db config

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,11 +9,6 @@ export default {
     baseURL,
   },
   /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
-  mode: 'universal',
-  /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
